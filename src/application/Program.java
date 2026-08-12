34d1dbeaf82e8ea7e8366fa57f25a76f6e7bff7c
Benjamin Schill <benjamin.schill.dev@gmail.com>
2026-08-12 11:47:09 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,25 +1,17 @@
 package application;
 
-import java.time.LocalDate;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
-		
-		Seller seller = new Seller(21, "Bob", "Bob@gmail.com", LocalDate.now(), 3000.0, obj);
-		System.out.println(seller);
-		
+
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
 		
 	}
-
 }
